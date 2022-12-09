ad1837c094891fa5af7bc8b8d452813f0dd51dc7
Nastya <tsutskova.an1996@yandex.ru>
2022-12-09 17:47:36 +0300

    feat: создать метод main и объекты для работы

--- src/Main.java
+++ src/Main.java
@@ -2,36 +2,20 @@ public class Main {
 
     public static void main(String[] args) {
         Manager manager = new Manager();
-        Task task1 = new Task("Решить задачу", "Решить задачу в Практикуме", manager.countId(),
-                "To do");
+        Task task1 = new Task("Практикум", "Решить задачу", manager.countId(),
+                "New");
+
         Epic epic1 = new Epic("День рождение", "Организовать др", manager.countId(), "");
         SubTask subTask1 = new SubTask("Ресторан", "Посмотреть рестораны рядом", manager.countId(),
-                "Done", epic1.id);
+                "New", epic1.id);
         SubTask subTask1_1 = new SubTask("Гости", "Позвать гостей", manager.countId(), "New",
                 epic1.id);
 
-        Epic epic2 = new Epic("Свадьба", "Организовать свадьбу", manager.countId(), "");
-        SubTask subTask2 = new SubTask("Найти молодожен", "Посмореть вокруг", manager.countId(),
-                "Done", epic2.id);
+        Epic epic2 = new Epic("Квартира", "Продать квартиру", manager.countId(), "");
+        SubTask subTask2 = new SubTask("Встретиться с риелтором", "Отдать ключи", manager.countId(),
+                "New", epic2.id);
 
-        manager.create(task1);
-        manager.create(epic1);
-        manager.create(epic2);
-        manager.create(subTask1);
-        manager.create(subTask1_1);
-        manager.create(subTask2);
-
-        System.out.println(manager.getEpicSubTasks(epic1.id));
-
-        manager.getAllTasks();
-        System.out.println(manager.tasksList);
-        System.out.println(manager.epicsList);
-        System.out.println(manager.subTasksList);
-
-        manager.deleteById(5);
-        System.out.println(epic1);
-        System.out.println(epic2);
-
+        Epic epic3 = new Epic("Аэропорт", "Встретить друга", manager.countId(), "");
 
     }
 }
